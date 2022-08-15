c84c06916ec8a61d34e59838dfd2232afe8e2018
justVovka <vovkamokroff@gmail.com>
2022-08-15 16:05:37 +0300

    remove aliases

--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,6 @@ module.exports = {
   },
   resolve: {
     modules: [path.join(__dirname, '/src'), 'node_modules'],
-    alias: {
-      '@components': path.resolve(srcDir, '/src/components/'),
-      '@pages': path.resolve(srcDir, '/src/pages/'),
-      '@styles': path.resolve(srcDir, '/src/styles/'),
-      '@api': path.resolve(srcDir, '/src/api/'),
-      '@store': path.resolve(srcDir, '/src/store/'),
-      '@utils': path.resolve(srcDir, '/src/utils/'),
-      '@helpers': path.resolve(srcDir, '/src/helpers/'),
-    },
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   module: {
